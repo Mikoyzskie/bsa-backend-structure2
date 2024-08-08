1891b04cf017117052cc4c56113c4f6f5c230a00
Mikoyzskie <rapidstrike13@gmail.com>
2024-08-08 15:25:26 +0800

    validation changes

--- index.js
+++ index.js
@@ -21,9 +21,13 @@ const {
   validateUserUpdate,
 } = require("./middlewares/validations/user.validation");
 const {
-  validateEventCreate,
   validateEventUpdate,
-} = require("./middlewares/validations/event.validation");
+  validateEventCreate,
+} = require("./middlewares/validations/events/event.validation");
+
+const { EventValidation } = require("./middlewares/validations/validation");
+
+// const { validateEventCreate } = require("./middlewares/validations/validation");
 
 var dbConfig = require("./knexfile");
 var app = express();
@@ -181,72 +185,77 @@ app.post(
   }
 );
 
-app.post("/events", validateEventCreate, authMiddlewareAdmin, (req, res) => {
-  try {
-    req.body.odds.home_win = req.body.odds.homeWin;
-    delete req.body.odds.homeWin;
-    req.body.odds.away_win = req.body.odds.awayWin;
-    delete req.body.odds.awayWin;
-    db("odds")
-      .insert(req.body.odds)
-      .returning("*")
-      .then(([odds]) => {
-        delete req.body.odds;
-        req.body.away_team = req.body.awayTeam;
-        req.body.home_team = req.body.homeTeam;
-        req.body.start_at = req.body.startAt;
-        delete req.body.awayTeam;
-        delete req.body.homeTeam;
-        delete req.body.startAt;
-        db("event")
-          .insert({
-            ...req.body,
-            odds_id: odds.id,
-          })
-          .returning("*")
-          .then(([event]) => {
-            statEmitter.emit("newEvent");
-            [
-              "bet_amount",
-              "event_id",
-              "away_team",
-              "home_team",
-              "odds_id",
-              "start_at",
-              "updated_at",
-              "created_at",
-            ].forEach((whatakey) => {
-              var index = whatakey.indexOf("_");
-              var newKey = whatakey.replace("_", "");
-              newKey = newKey.split("");
-              newKey[index] = newKey[index].toUpperCase();
-              newKey = newKey.join("");
-              event[newKey] = event[whatakey];
-              delete event[whatakey];
-            });
-            ["home_win", "away_win", "created_at", "updated_at"].forEach(
-              (whatakey) => {
+app.post(
+  "/events",
+  EventValidation.validateEventCreate,
+  authMiddlewareAdmin,
+  (req, res) => {
+    try {
+      req.body.odds.home_win = req.body.odds.homeWin;
+      delete req.body.odds.homeWin;
+      req.body.odds.away_win = req.body.odds.awayWin;
+      delete req.body.odds.awayWin;
+      db("odds")
+        .insert(req.body.odds)
+        .returning("*")
+        .then(([odds]) => {
+          delete req.body.odds;
+          req.body.away_team = req.body.awayTeam;
+          req.body.home_team = req.body.homeTeam;
+          req.body.start_at = req.body.startAt;
+          delete req.body.awayTeam;
+          delete req.body.homeTeam;
+          delete req.body.startAt;
+          db("event")
+            .insert({
+              ...req.body,
+              odds_id: odds.id,
+            })
+            .returning("*")
+            .then(([event]) => {
+              statEmitter.emit("newEvent");
+              [
+                "bet_amount",
+                "event_id",
+                "away_team",
+                "home_team",
+                "odds_id",
+                "start_at",
+                "updated_at",
+                "created_at",
+              ].forEach((whatakey) => {
                 var index = whatakey.indexOf("_");
                 var newKey = whatakey.replace("_", "");
                 newKey = newKey.split("");
                 newKey[index] = newKey[index].toUpperCase();
                 newKey = newKey.join("");
-                odds[newKey] = odds[whatakey];
-                delete odds[whatakey];
-              }
-            );
-            return res.send({
-              ...event,
-              odds,
+                event[newKey] = event[whatakey];
+                delete event[whatakey];
+              });
+              ["home_win", "away_win", "created_at", "updated_at"].forEach(
+                (whatakey) => {
+                  var index = whatakey.indexOf("_");
+                  var newKey = whatakey.replace("_", "");
+                  newKey = newKey.split("");
+                  newKey[index] = newKey[index].toUpperCase();
+                  newKey = newKey.join("");
+                  odds[newKey] = odds[whatakey];
+                  delete odds[whatakey];
+                }
+              );
+              return res.send({
+                ...event,
+                odds,
+              });
             });
-          });
-      });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Internal Server Error");
-    return;
+        });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
   }
-});
+);
 
 app.post("/bets", betValidation, authMiddleware, (req, res) => {
   let userId = req.tokenPayload.id;
--- middlewares/validations/validation.js
+++ middlewares/validations/validation.js
@@ -1,16 +1,3 @@
-class ValidationMiddleware {
-  constructor({ schema, content }) {
-    this.schema = schema;
-    this.content = content;
-  }
+const EventValidation = require("./events/event.validation");
 
-  validateSchema(req, res, next) {
-    const { error } = this.schema.validate(this.content);
-    if (error) {
-      return res.status(400).send({ error: error.details[0].message });
-    }
-    next();
-  }
-}
-
-module.exports = new ValidationMiddleware();
+module.exports = { EventValidation };
